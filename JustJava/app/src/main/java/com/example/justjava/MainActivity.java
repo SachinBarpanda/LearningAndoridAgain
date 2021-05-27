6453be52618463e82ce4ae4fe1400809210961b2
SachinBarpanda <sachinongithub@gmail.com>
2021-05-27 23:55:44 +0530

    just adding a change in ui and variables

--- JustJava/app/src/main/java/com/example/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/justjava/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.text.NumberFormat;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,11 +17,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void submitOrders(View view) {
-        display(1);
+        int numberOfCoffees = 2;
+        display(numberOfCoffees);
+        displayPrice(numberOfCoffees * 5);
     }
 
     public void display(int i){
         TextView quantityText = findViewById(R.id.quantity_text_view);
         quantityText.setText(String.valueOf(i));
+    }
+
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
