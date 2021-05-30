c02e653ec39f5e683037f60fdf33d7a95ffa205b
SachinBarpanda <sachinongithub@gmail.com>
2021-05-30 23:52:38 +0530

    Global variable is made and intialisation of textview is made.

--- JustJava/app/src/main/java/com/example/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/justjava/MainActivity.java
@@ -10,35 +10,41 @@ import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView quantityText;
+    private int quantity = 2;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        quantityText = findViewById(R.id.quantity_text_view);
+        quantityText.setText(quantity+"");
     }
 
     public void submitOrder(View view) {
-        int quantity = 5;
+
         display(quantity);
         displayPrice(quantity * 5);
     }
 
     public void display(int i){
-        TextView quantityText = findViewById(R.id.quantity_text_view);
+
         quantityText.setText(String.valueOf(i));
     }
 
     private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        TextView priceTextView = findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
     public void decrement(View view) {
-        int quantity = 1;
+        quantity = quantity - 1;
         display(quantity);
     }
 
     public void increment(View view) {
-        int quantity =3;
+        quantity = quantity + 1;
         display(quantity);
     }
 }
